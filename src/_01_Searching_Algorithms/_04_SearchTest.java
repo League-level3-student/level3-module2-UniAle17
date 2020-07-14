1f62b7e01194e74504692aa1c79cb91237eadfea
root <root@f8b756ac05ff>
2020-07-14 00:43:00 +0000

    SearchTest

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,21 +11,34 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[] testWords = {"One", "Two", "Three", "Four"};
 		
-		assertEquals
-		
+		assertEquals(1, _00_LinearSearch.linearSearch(testWords, "Two"));
+		assertEquals(3, _00_LinearSearch.linearSearch(testWords, "Four"));
+		assertEquals(-1, _00_LinearSearch.linearSearch(testWords, "Six"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		
+		int[] testNumbers = {1, 2, 3, 4};
+		
+		assertEquals(0, _01_BinarySearch.binarySearch(testNumbers, 0, testNumbers.length-1, 1));
+		assertEquals(2, _01_BinarySearch.binarySearch(testNumbers, 0, testNumbers.length-1, 3));
+		assertEquals(-1, _01_BinarySearch.binarySearch(testNumbers, 0, testNumbers.length-1, 5));
+		
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		
+		
+		
+		
 	}
 	
 	@Test
